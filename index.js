3525b826c13ec3c497077c5c549a73742f767b75
Anton Gudenko <antong702@gmail.com>
2024-12-11 16:38:26 +0300

    changes in index.js x2

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import { registerRootComponent } from 'expo';
 import TrackPlayer from 'react-native-track-player';
 import { ExpoRoot } from 'expo-router';
 
-import App from './App';
-
 export function App() {
 	const ctx = require.context('./app');
 	return <ExpoRoot context={ctx} />;
