46224f301af4c30ed6aba37f7f050f9e8bf7a020
Anton Gudenko <antong702@gmail.com>
2024-12-11 16:21:35 +0300

    changes in app.json and package.json

--- utils/trackPlayerService.js
+++ utils/trackPlayerService.js
@@ -1,6 +1,3 @@
 import TrackPlayer, { Event } from 'react-native-track-player';
 
-export default async function () {
-	TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
-	TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
-}
+export default async function () {}
