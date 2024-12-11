bd9bdc792f5401219975f60c53962ff778f75ab3
Anton Gudenko <antong702@gmail.com>
2024-12-11 18:53:48 +0300

    changes in index.js x6

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-import 'expo-router/entry';
+import { registerRootComponent } from 'expo';
 import TrackPlayer from 'react-native-track-player';
-import trackPlayerService from './utils/trackPlayerService';
 
-TrackPlayer.registerPlaybackService(trackPlayerService);
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
+TrackPlayer.registerPlaybackService(() =>
+	require('./utils/trackPlayerService')
+);
